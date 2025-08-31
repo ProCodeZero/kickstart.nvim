5b89f283f0faad433abbb979d96c792b14527fa6
HollowDS <denis.osipov.2288.02@gmail.com>
2025-08-31 09:26:43 +0300

    set tabwight to 4

--- init.lua
+++ init.lua
@@ -80,6 +80,12 @@ vim.o.scrolloff = 10
 -- See `:help 'confirm'`
 vim.o.confirm = true
 
+-- tabs length
+vim.o.tabstop = 4
+vim.o.expandtab = true
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
+
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
