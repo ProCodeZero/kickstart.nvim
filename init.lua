d59060611cee3f41ed9238c07002a73c2bd12c1f
HollowDS <denis.osipov.2288.02@gmail.com>
2026-02-13 21:33:01 +0300

    scheme to tokyonight

--- init.lua
+++ init.lua
@@ -730,14 +730,27 @@ require('lazy').setup({
     },
   },
   -- colorscheme
+  -- {
+  --  'sainnhe/everforest',
+  -- lazy = false,
+  -- priority = 1000,
+  -- config = function()
+  --  vim.g.everforest_background = 'hard'
+  -- vim.g.everforest_enable_italic = true
+  --   vim.cmd.colorscheme 'everforest'
+  --   end,
+  --  },
   {
-    'sainnhe/everforest',
-    lazy = false,
+    'folke/tokyonight.nvim',
     priority = 1000,
     config = function()
-      vim.g.everforest_background = 'hard'
-      vim.g.everforest_enable_italic = true
-      vim.cmd.colorscheme 'everforest'
+      ---@diagnostic disable-next-line: missing-fields
+      require('tokyonight').setup {
+        styles = {
+          comments = { italic = false },
+        },
+      }
+      vim.cmd.colorscheme 'tokyonight-night'
     end,
   },
 
