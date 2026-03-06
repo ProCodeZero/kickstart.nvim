3e9cc68ca5cd7ab2f0a8615aab9d08daa9eb77b6
HollowDS <denis.osipov.2288.02@gmail.com>
2026-03-06 18:26:14 +0300

    switch harpoon to global repo

--- lua/theprimeagen/lazy/local.lua
+++ lua/theprimeagen/lazy/local.lua
@@ -58,38 +58,38 @@ local local_plugins = {
 	-- 	dir = "~/personal/the-stru",
 	-- },
 
-	{
-		"harpoon",
-		dir = "~/neovim/harpoon",
-		config = function()
-			local harpoon = require("harpoon")
+	-- {
+	-- 	"harpoon",
+	-- 	dir = "~/neovim/harpoon",
+	-- 	config = function()
+	-- 		local harpoon = require("harpoon")
 
-			harpoon:setup()
+	-- 		harpoon:setup()
 
-			vim.keymap.set("n", "<leader>A", function()
-				harpoon:list():prepend()
-			end)
-			vim.keymap.set("n", "<leader>a", function()
-				harpoon:list():add()
-			end)
-			vim.keymap.set("n", "<C-e>", function()
-				harpoon.ui:toggle_quick_menu(harpoon:list())
-			end)
+	-- 		vim.keymap.set("n", "<leader>A", function()
+	-- 			harpoon:list():prepend()
+	-- 		end)
+	-- 		vim.keymap.set("n", "<leader>a", function()
+	-- 			harpoon:list():add()
+	-- 		end)
+	-- 		vim.keymap.set("n", "<C-e>", function()
+	-- 			harpoon.ui:toggle_quick_menu(harpoon:list())
+	-- 		end)
 
-			vim.keymap.set("n", "<M-1>", function()
-				harpoon:list():select(1)
-			end)
-			vim.keymap.set("n", "<M-2>", function()
-				harpoon:list():select(2)
-			end)
-			vim.keymap.set("n", "<M-3>", function()
-				harpoon:list():select(3)
-			end)
-			vim.keymap.set("n", "<M-4>", function()
-				harpoon:list():select(4)
-			end)
-		end,
-	},
+	-- 		vim.keymap.set("n", "<M-1>", function()
+	-- 			harpoon:list():select(1)
+	-- 		end)
+	-- 		vim.keymap.set("n", "<M-2>", function()
+	-- 			harpoon:list():select(2)
+	-- 		end)
+	-- 		vim.keymap.set("n", "<M-3>", function()
+	-- 			harpoon:list():select(3)
+	-- 		end)
+	-- 		vim.keymap.set("n", "<M-4>", function()
+	-- 			harpoon:list():select(4)
+	-- 		end)
+	-- 	end,
+	-- },
 }
 
 return local_plugins
